1b0b95d7efa1bd400ce0a15e5478f33ff50969f5
Godari <srinivas.godari@accenture.com>
2020-05-05 01:26:38 +0530

    simpl code

---
+++ src/main/java/test/resources/UCS.java
@@ -0,0 +1,16 @@
+package resources;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class UCS{
+     @Test
+      public void testAdd() {
+     assertTrue(false);
+      }
+      
+      @Test
+      public void testSub() {
+     assertTrue(true);
+      }
+}
