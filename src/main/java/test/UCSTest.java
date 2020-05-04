b2acd0b4538e922f391335e09df975feb0811158
Godari <srinivas.godari@accenture.com>
2020-05-05 01:49:50 +0530

    gfg gfgh

---
+++ src/main/java/test/UCSTest.java
@@ -0,0 +1,21 @@
+package test;
+import static org.junit.Assert.*;
+
+ 
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+public class UCSTest{
+     @Test
+      public void testAdd() {
+     assertTrue(false);
+      }
+      
+      @Test
+      public void testSub() {
+     assertTrue(true);
+      }
+}
